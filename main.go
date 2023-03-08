72f366ee57362123477d32adfaf8afea0df4508d
jeavaljean <jeanvaljean0520@gmail.com>
2023-03-08 21:23:15 +0900

    [add] :: data type 추가

--- main.go
+++ main.go
@@ -4,15 +4,11 @@ package main
 // import는 기본 패키지가 아닌 외부에 있는 다른 패키지를 포함 시키기 위해 필요하다.
 import (
 	"fmt"
+	"reflect"
 )
 
 // 매인 함수
 func main() {
-	//다중 선언
-	message, hi, whoAreYou, goodBye := "hello", "hi", "who are you", "goodBye"
-
-	fmt.Println(message)
-	fmt.Println(hi)
-	fmt.Println(whoAreYou)
-	fmt.Println(goodBye)
+	golang, java := "Sexy", false
+	fmt.Println(reflect.TypeOf(golang), reflect.TypeOf(java))
 }
