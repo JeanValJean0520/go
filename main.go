853fd52fa72fe013980096d5565c860d1fb649eb
jeavaljean <jeanvaljean0520@gmail.com>
2023-03-08 21:14:39 +0900

    [add] :: 다중 선언

--- main.go
+++ main.go
@@ -4,24 +4,15 @@ package main
 // import는 기본 패키지가 아닌 외부에 있는 다른 패키지를 포함 시키기 위해 필요하다.
 import (
 	"fmt"
-	"reflect"
 )
 
 // 매인 함수
 func main() {
-	// fmt 라는 외부 패키지의 기능을 갖고와서 사용
-	fmt.Println("Hello world")
+	//다중 선언
+	message, hi, whoAreYou, goodBye := "hello", "hi", "who are you", "goodBye"
 
-	message := []byte{'H', 'J'}
-	var message_a [3]int = [3]int{1, 2, 3}
-
-	fmt.Println(string(message))
-	fmt.Println(message_a)
-
-	var stringA string
-	stringA = "Hello DW"
-	fmt.Println(reflect.TypeOf(stringA))
-
-	stringB := "StringB"
-	fmt.Println(reflect.TypeOf(stringB))
+	fmt.Println(message)
+	fmt.Println(hi)
+	fmt.Println(whoAreYou)
+	fmt.Println(goodBye)
 }
