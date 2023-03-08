b73554186cacbd82fb8baa765242e5cad958bbfd
jeavaljean <jeanvaljean0520@gmail.com>
2023-03-08 19:41:17 +0900

    [add] :: 기본 구조에 대한 주석 추가

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
+// 변수, 함수, 상수, 메서드 등등을 패키지로 갖고온다.
 package main
 
+// import는 기본 패키지가 아닌 외부에 있는 다른 패키지를 포함 시키기 위해 필요하다.
 import (
 	"fmt"
 )
 
+// 매인 함수
 func main() {
+	// fmt 라는 외부 패키지의 기능을 갖고와서 사용
 	fmt.Println("Hello world")
 }
