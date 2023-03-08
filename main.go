faa581a0695984f54d6362364c74954c4936c8ee
jeavaljean <jeanvaljean0520@gmail.com>
2023-03-08 22:49:31 +0900

    [add] :: 신규 패키지 추가

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ package main
 // import는 기본 패키지가 아닌 외부에 있는 다른 패키지를 포함 시키기 위해 필요하다.
 import (
 	"fmt"
-	"reflect"
+	"time"
 )
 
-// 매인 함수
 func main() {
-	golang, java := "Sexy", false
-	fmt.Println(reflect.TypeOf(golang), reflect.TypeOf(java))
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("goodBye")
+	case t.Hour() < 17:
+		fmt.Println("gooo")
+	default:
+		fmt.Println("de")
+	}
 }
